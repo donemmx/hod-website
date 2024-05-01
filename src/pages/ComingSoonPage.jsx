ab4ea7e250027cd99049cd1878a9d90688f485ca
donemmx <emmanuelidus@gmail.com>
2024-05-01 12:41:01 +0100

    fix

---
+++ src/pages/ComingSoonPage.jsx
@@ -0,0 +1,20 @@
+import ComingSoonSection from "@sections/coming-soon/ComingSoonSection";
+
+const ComingSoonPage = () => {
+  return (
+    <>
+      <a
+        href="#"
+        className="d-flex justify-content-center section-padding-top-120"
+      >
+        {/* <img src="/image/logo.png" alt="image alt" /> */}
+        <h3 className="font-bold">HEAD OF DAYS LTD</h3>
+
+      </a>
+
+      <ComingSoonSection />
+    </>
+  );
+};
+
+export default ComingSoonPage;
---
+++ src/sections/coming-soon/ComingSoonSection.jsx
@@ -0,0 +1,37 @@
+import CountDown from "./CountDown/CountDown";
+
+const ComingSoonSection = () => {
+  return (
+    <>
+      <section className="coming-soon-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-7">
+              <div className="coming-soon_content-wrapper">
+                {/* <CountDown /> */}
+
+                <h2 className="heading-md text-black">
+                  We're Curently Under Maintance!
+                </h2>
+                <div className="coming-soon_newsletter">
+                  <div className="newsletter-form-1">
+                    <input
+                      type="email"
+                      placeholder="Enter your email"
+                      className="form-control"
+                    />
+                    <button className="btn-masco rounded-pill">
+                      Get Notified
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ComingSoonSection;
